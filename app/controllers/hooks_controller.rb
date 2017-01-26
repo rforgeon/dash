2d375c4e2ceda775e734d1ac5242b08d2afde085
rforgeon <rforgeon@gmail.com>
2017-01-26 09:06:49 -0800

    hooks updated

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -5,26 +5,27 @@ class HooksController
 
 
     # If the body contains the pickup parameter...
-    if params[:pickup].present?
+    if params['pickup'].present?
+      #if parmas['event_id'].isUnique
       # Create a new Survey object based on the received parameters...
-      lyftRide = Lyft_Metric.new(:ride_id => params[:ride_id])
+      lyftRide = Lyft_Metric.new(:ride_id => params['ride_id'])
 
       #lyftRide.user_id = current_user.id ##not sure how to capture this
-      lyftRide.eta_seconds = params[:destination][:eta_seconds]
-      lyftRide.pickup_time = params[:pickup][:time]
-      lyftRide.ride_type = params[:ride_type]
-      lyftRide.primetime_percentage = params[:primetime_percentage]
+      lyftRide.eta_seconds = params['destination']['eta_seconds']
+      # lyftRide.pickup_time = params[:pickup][:time]
+      # lyftRide.ride_type = params[:ride_type]
+      # lyftRide.primetime_percentage = params[:primetime_percentage]
 
       lyftRide.save
 
     end
 
     if params[:dropoff].present?
-      lyftRide = Lyft_Metric.find_by ride_id: params[ride_id]
+      lyftRide = Lyft_Metric.find_by ride_id: params['ride_id']
 
-      lyftRide.dropoff_time = params[:dropoff][:time]
-      actualRideSeconds = ((lyftRide.dropoff_time.to_datetime - lyftRide.pickup_time.to_datetime) * 24 * 60 * 60).to_i
-      lyftRide.eta_dif_seconds = actualRideSeconds - lyftRide.eta_seconds
+      lyftRide.dropoff_time = params['dropoff']['time']
+      #actualRideSeconds = ((lyftRide.dropoff_time.to_datetime - lyftRide.pickup_time.to_datetime) * 24 * 60 * 60).to_i
+      #lyftRide.eta_dif_seconds = actualRideSeconds - lyftRide.eta_seconds
 
     end
 
