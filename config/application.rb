489374294c97282c8a92d2d9c20862406becc57d
rforgeon <rforgeon@gmail.com>
2017-02-13 15:49:50 -0800

    added back cors

--- config/application.rb
+++ config/application.rb
@@ -30,15 +30,15 @@ module FlashDash
     # Skip views, helpers and assets when generating a new resource.
   #   config.api_only = true
    #
-  #  config.middleware.use Rack::Cors do
-  #    allow do
-  #      origins '*'
-  #      resource '*',
-  #       :headers => :any,
-  #       :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-  #       :methods => [:get, :post, :options, :delete, :put]
-  #    end
-  #  end
+   config.middleware.use Rack::Cors do
+     allow do
+       origins '*'
+       resource '*',
+        :headers => :any,
+        :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+        :methods => [:get, :post, :options, :delete, :put]
+     end
+   end
 
    #
   #  config.middleware.insert_before(Warden::Manager, ActionDispatch::Cookies)
