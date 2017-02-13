985180a666aec2e34230501c0880ba84e33aee58
rforgeon <rforgeon@gmail.com>
2017-02-13 15:02:41 -0800

    lyft params updated

--- app/controllers/api/lyft_controller.rb
+++ app/controllers/api/lyft_controller.rb
@@ -2,7 +2,7 @@ class Api::LyftController < ApplicationController
 
   #before_action :authenticate_user!
 
-  def show
+  def show(params)
     @lyft_identity = User.find params[:id]
     token = (@lyft_identity.lyft_token).to_s
     render json: {'access-token':token}
