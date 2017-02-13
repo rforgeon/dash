14d69873c53d0a4419427ef990e83a9874732513
rforgeon <rforgeon@gmail.com>
2017-02-13 14:44:41 -0800

    params blank added

--- app/controllers/api/yelp_controller.rb
+++ app/controllers/api/yelp_controller.rb
@@ -4,14 +4,16 @@ class Api::YelpController < ApplicationController
   require 'json'
 
 
-  def search
+  def search (params = {})
+    # @params = params
     var lat = params['lat']
     var lng = params['lng']
     coordinates = {latitude: lat, longitude: lng }
     render json: Yelp.client.search_by_coordinates(coordinates, { limit: 3, radius_filter: 30 })
   end
 
-  def search_recs
+  def search_recs (params = {})
+    # @params = params
     var location = params['location']
     var category = params['category']
 
@@ -19,9 +21,9 @@ class Api::YelpController < ApplicationController
 
   end
 
-  def params
-    @params
-  end  
+  # def params
+  #   @params
+  # end
 
 
 end
